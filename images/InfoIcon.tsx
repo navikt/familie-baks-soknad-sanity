b82a685ab0a195a0dc23142ef37dd5662837fb7a
Daphne <daphne.leebeek@nav.no>
2022-08-10 16:36:14 +0200

    Fix/deploy (#12)
    * Ikke importer logoer fra navikt
    
    * Test deploy
    
    * Deploy kun på master

---
+++ images/InfoIcon.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const InfoIcon: React.FC = () => (
+  <svg
+    width="1.3em"
+    height="1.3em"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    focusable="false"
+    role="img"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M12 0C5.382 0 0 5.382 0 12s5.382 12 12 12c6.617 0 12-5.382 12-12S18.617 0 12 0Z"
+      fill="#368DA8"
+    ></path>
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M13.5 6.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0ZM9 17v2h6v-2h-2v-7H9v2h2v5H9Z"
+      fill="#fff"
+    ></path>
+  </svg>
+);
+
+export default InfoIcon;
---
+++ images/WarningIcon.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const WarningIcon: React.FC = () => (
+  <svg
+    width="1.3em"
+    height="1.3em"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    focusable="false"
+    role="img"
+  >
+    <path
+      d="M11.126.564a.962.962 0 0 1 1.741-.021l11.011 21.912c.358.695-.117 1.54-.865 1.54H.987c-.738 0-1.215-.826-.876-1.519L11.126.564Z"
+      fill="#D47B00"
+    ></path>
+    <path d="M11 7.996h2v7h-2v-7Z" fill="#fff"></path>
+    <circle cx="12" cy="18.496" r="1.5" fill="#fff"></circle>
+  </svg>
+);
+
+export default WarningIcon;
