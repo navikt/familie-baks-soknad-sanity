bd738eab06a3a711b8c23550f260ee709cb8928d
Daphne Leebeek <daphne.leebeek@nav.no>
2022-08-15 13:24:02 +0200

    Samle dokumenter til steg din livssituasjon i en export

---
+++ schemas/steg/din-livssituasjon/dinLivssituasjonDokumenter.ts
@@ -0,0 +1,7 @@
+import { Steg } from '../../typer';
+import stegTittel from '../stegTittel';
+import spørsmål from './spørsmål';
+
+const dinLivssituasjonDokumenter = [stegTittel(Steg.DIN_LIVSSITUASJON), spørsmål];
+
+export default dinLivssituasjonDokumenter;
