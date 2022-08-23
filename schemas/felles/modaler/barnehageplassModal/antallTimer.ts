73c4dd514c61e0c85df815dd078407ea28a2fb0f
norafutsaeter <nora.futseter@nav.no>
2022-08-23 10:37:24 +0200

    Fjerne lag på antallTimer

--- schemas/felles/modaler/barnehageplassModal/antallTimer.ts
+++ schemas/felles/modaler/barnehageplassModal/antallTimer.ts
@@ -2,39 +2,26 @@ import { CustomSanityTyper, Field, SanityTyper } from '../../../typer';
 
 const antallTimer: Field[] = [
   {
-    title: 'Antall timer',
+    title: 'Hvor mange timer i uken har barnet fått tildelt barnehageplass?',
     name: 'antallTimer',
     type: SanityTyper.OBJECT,
-    options: {
-      collapsable: true,
-      collapsed: true,
-    },
-    validation: Rule => Rule.required().error('Du må fylle inn spørsmål om antall timer'),
+    description: '(obligatorisk)',
+    validation: Rule => Rule.required().error('Du må fylle inn spørsmål om timer i barnehagen'),
     fields: [
       {
-        title: 'Hvor mange timer i uken har barnet fått tildelt barnehageplass?',
-        name: 'antallTimer',
-        type: SanityTyper.OBJECT,
-        description: '(obligatorisk)',
+        title: 'Spørsmål',
+        name: 'sporsmal',
+        type: CustomSanityTyper.LOCALE_STRING,
         validation: Rule => Rule.required().error('Du må fylle inn spørsmål om timer i barnehagen'),
-        fields: [
-          {
-            title: 'Spørsmål',
-            name: 'sporsmal',
-            type: CustomSanityTyper.LOCALE_STRING,
-            validation: Rule =>
-              Rule.required().error('Du må fylle inn spørsmål om timer i barnehagen'),
-          },
-          {
-            title: 'Feilmelding',
-            name: 'feilmelding',
-            type: CustomSanityTyper.LOCALE_STRING,
-            validation: Rule =>
-              Rule.required().error(
-                'Du må fylle inn en feilmelding for spørsmål om timer i barnehagen',
-              ),
-          },
-        ],
+      },
+      {
+        title: 'Feilmelding',
+        name: 'feilmelding',
+        type: CustomSanityTyper.LOCALE_STRING,
+        validation: Rule =>
+          Rule.required().error(
+            'Du må fylle inn en feilmelding for spørsmål om timer i barnehagen',
+          ),
       },
     ],
   },
