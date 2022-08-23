574457fd6b82d66d5e5189fb50f4fc35f32f4a22
Daphne Leebeek <daphne.leebeek@nav.no>
2022-08-23 16:40:21 +0200

    legg til flettefelt for antall

--- schemas/steg/om-barnet/tittel.ts
+++ schemas/steg/om-barnet/tittel.ts
@@ -1,11 +1,22 @@
 import stegTittelDokument from '../../felles/stegTittelDokument';
-import { DokumentNavn, dokumentTittel, StegDokument, Steg } from '../../typer';
+import { DokumentNavn, dokumentTittel, StegDokument, Steg, CustomSanityTyper } from '../../typer';
 
 const tittel: StegDokument = {
   ...stegTittelDokument,
   steg: Steg.OM_BARNET,
   title: dokumentTittel.OM_BARNET_TITTEL,
   name: DokumentNavn.OM_BARNET_TITTEL,
+  type: stegTittelDokument.type,
+  fields: [
+    ...stegTittelDokument.fields,
+    {
+      title: 'Subtittel',
+      name: 'subtittel',
+      type: CustomSanityTyper.LOCALE_BLOCK,
+      description: '"Barn 1 av 2" (obligatorisk)',
+      validation: rule => rule.required().error('Du må fylle inn subtittel til steget'),
+    },
+  ],
 };
 
 export default tittel;
