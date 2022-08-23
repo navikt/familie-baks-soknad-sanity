ec8ecbbd8eca9e4454beaabc72e48400bbbfa444
Daphne Leebeek <daphne.leebeek@nav.no>
2022-08-23 12:25:59 +0200

    Lag arbeidsperiodemodal per person

--- schemas/felles/modaler/modalerDokumenter.ts
+++ schemas/felles/modaler/modalerDokumenter.ts
@@ -1,8 +1,8 @@
 import { DokumentNavn, dokumentTittel } from '../../typer';
 import arbeidsperiodeModal from './arbeidsperiodeModal/arbeidsperiodeModal';
 import barnehageplassModal from './barnehageplassModal/barnehageplassModal';
-import pensjonsModal from './pensjonsModal/pensjonsModal';
 import eosYtelseModal from './eøsYtelseModal/eosYtelseModal';
+import pensjonsModal from './pensjonsModal/pensjonsModal';
 import utenlandsoppholdModal from './utenlandsoppholdModal/utenlandsoppholdModal';
 import leggTilBarnModal from './velgBarnModal/leggTilBarnModal';
 
@@ -19,7 +19,21 @@ const modalerDokumenter = [
   },
   barnehageplassModal,
   pensjonsModal,
-  arbeidsperiodeModal,
+  {
+    name: DokumentNavn.MODAL_ARBEIDSPERIODE_SOKER,
+    title: dokumentTittel.MODAL_ARBEIDSPERIODE_SOKER,
+    ...arbeidsperiodeModal,
+  },
+  {
+    name: DokumentNavn.MODAL_ARBEIDSPERIODE_ANDRE_FORELDER,
+    title: dokumentTittel.MODAL_ARBEIDSPERIODE_ANDRE_FORELDER,
+    ...arbeidsperiodeModal,
+  },
+  {
+    name: DokumentNavn.MODAL_ARBEIDSPERIODE_OMSORGSPERSON,
+    title: dokumentTittel.MODAL_ARBEIDSPERIODE_OMSORGSPERSON,
+    ...arbeidsperiodeModal,
+  },
   leggTilBarnModal,
   eosYtelseModal,
 ];
