1ef0100da6f2e981dacbb19452c6cd2882dc201a
Daphne Leebeek <daphne.leebeek@nav.no>
2022-08-23 16:02:32 +0200

    Legg til resten av oppfølgningsspørsmål

--- schemas/steg/om-barnet/oppfolgning/adopsjon.ts
+++ schemas/steg/om-barnet/oppfolgning/adopsjon.ts
@@ -1,4 +1,11 @@
-import { DokumentNavn, dokumentTittel, SanityTyper, Steg, StegDokument } from '../../../typer';
+import {
+  CustomSanityTyper,
+  DokumentNavn,
+  dokumentTittel,
+  SanityTyper,
+  Steg,
+  StegDokument,
+} from '../../../typer';
 import opplysningspaminnelse from './opplysningspaminnelse';
 
 const adopsjon: StegDokument = {
@@ -6,7 +13,35 @@ const adopsjon: StegDokument = {
   title: dokumentTittel.OM_BARNET_ADOPSJON,
   name: DokumentNavn.OM_BARNET_ADOPSJON,
   type: SanityTyper.DOCUMENT,
-  fields: [opplysningspaminnelse],
+  fields: [
+    opplysningspaminnelse,
+    {
+      name: 'foreldrepengerEllerEngangsstonad',
+      title: 'Utbetaling av foreldrepenger eller engangstønad',
+      type: SanityTyper.OBJECT,
+      options: {
+        collapsable: true,
+        collapsed: true,
+      },
+      validation: Rule => Rule.required().error('Du må fylle inn spørsmålsteksten'),
+      fields: [
+        {
+          name: 'sporsmal',
+          title: 'Spørsmål',
+          type: CustomSanityTyper.LOCALE_STRING,
+          description: '(obligatorisk)',
+          validation: Rule => Rule.required().error('Du må fylle inn spørsmålsteksten'),
+        },
+        {
+          name: 'feilmelding',
+          title: 'Feilmelding',
+          description: '(obligatorisk)',
+          type: CustomSanityTyper.LOCALE_STRING,
+          validation: Rule => Rule.required().error('Du må fylle inn feilmelding'),
+        },
+      ],
+    },
+  ],
 };
 
 export default adopsjon;
--- schemas/steg/om-barnet/oppfolgning/ytelseFraEos.ts
+++ schemas/steg/om-barnet/oppfolgning/ytelseFraEos.ts
@@ -1,4 +1,11 @@
-import { DokumentNavn, dokumentTittel, SanityTyper, Steg, StegDokument } from '../../../typer';
+import {
+  CustomSanityTyper,
+  DokumentNavn,
+  dokumentTittel,
+  SanityTyper,
+  Steg,
+  StegDokument,
+} from '../../../typer';
 import opplysningspaminnelse from './opplysningspaminnelse';
 
 const ytelseFraEos: StegDokument = {
@@ -6,7 +13,87 @@ const ytelseFraEos: StegDokument = {
   title: dokumentTittel.OM_BARNET_YTELSE_FRA_EOS,
   name: DokumentNavn.OM_BARNET_YTELSE_FRA_EOS,
   type: SanityTyper.DOCUMENT,
-  fields: [opplysningspaminnelse],
+  fields: [
+    opplysningspaminnelse,
+    {
+      name: 'pagaendeSoknad',
+      title: 'Pågående søknad om ytelse fra eøs land',
+      type: SanityTyper.OBJECT,
+      options: {
+        collapsable: true,
+        collapsed: true,
+      },
+      validation: Rule => Rule.required().error('Du må fylle inn spørsmålsteksten'),
+      fields: [
+        {
+          name: 'sporsmal',
+          title: 'Spørsmål',
+          type: CustomSanityTyper.LOCALE_STRING,
+          description: '(obligatorisk)',
+          validation: Rule => Rule.required().error('Du må fylle inn spørsmålsteksten'),
+        },
+        {
+          name: 'feilmelding',
+          title: 'Feilmelding',
+          description: '(obligatorisk)',
+          type: CustomSanityTyper.LOCALE_STRING,
+          validation: Rule => Rule.required().error('Du må fylle inn feilmelding'),
+        },
+      ],
+    },
+    {
+      name: 'pagaendeSoknadLand',
+      title: 'Hvilket land har x pågående søknad fra',
+      type: SanityTyper.OBJECT,
+      options: {
+        collapsable: true,
+        collapsed: true,
+      },
+      validation: Rule => Rule.required().error('Du må fylle inn spørsmålsteksten'),
+      fields: [
+        {
+          name: 'sporsmal',
+          title: 'Spørsmål',
+          type: CustomSanityTyper.LOCALE_STRING,
+          description: '(obligatorisk)',
+          validation: Rule => Rule.required().error('Du må fylle inn spørsmålsteksten'),
+        },
+        {
+          name: 'feilmelding',
+          title: 'Feilmelding',
+          description: '(obligatorisk)',
+          type: CustomSanityTyper.LOCALE_STRING,
+          validation: Rule => Rule.required().error('Du må fylle inn feilmelding'),
+        },
+      ],
+    },
+    {
+      name: 'farYtelse',
+      title: 'Får/fått ytelse fra eøs land',
+      type: SanityTyper.OBJECT,
+      options: {
+        collapsable: true,
+        collapsed: true,
+      },
+      validation: Rule => Rule.required().error('Du må fylle inn spørsmålsteksten'),
+      fields: [
+        {
+          name: 'sporsmal',
+          title: 'Spørsmål',
+          type: CustomSanityTyper.LOCALE_STRING,
+          description: '(obligatorisk)',
+          validation: Rule => Rule.required().error('Du må fylle inn spørsmålsteksten'),
+        },
+        {
+          name: 'feilmelding',
+          title: 'Feilmelding',
+          description: '(obligatorisk)',
+          type: CustomSanityTyper.LOCALE_STRING,
+          validation: Rule => Rule.required().error('Du må fylle inn feilmelding'),
+        },
+      ],
+    },
+  ],
 };
 
 export default ytelseFraEos;
