aecbdc60b9bae4aab23a4b3ca6d3cdb5ccfadf38
norafutsaeter <nora.futseter@nav.no>
2022-08-23 07:39:25 +0200

    Endre til samme form for fortid, nåtid og fremtid

--- schemas/felles/modaler/barnehageplassModal/antallTimer.ts
+++ schemas/felles/modaler/barnehageplassModal/antallTimer.ts
@@ -12,8 +12,8 @@ const antallTimer: Field[] = [
     validation: Rule => Rule.required().error('Du må fylle inn spørsmål om antall timer'),
     fields: [
       {
-        title: 'Hvor mange timer i uken har x fått tildelt i barnehagen',
-        name: 'harAntallTimer',
+        title: 'Hvor mange timer i uken har barnet fått tildelt barnehageplass?',
+        name: 'antallTimer',
         type: SanityTyper.OBJECT,
         description: '(obligatorisk)',
         validation: Rule => Rule.required().error('Du må fylle inn spørsmål om timer i barnehagen'),
@@ -32,55 +32,6 @@ const antallTimer: Field[] = [
             validation: Rule =>
               Rule.required().error(
                 'Du må fylle inn en feilmelding for spørsmål om timer i barnehagen',
-              ),
-          },
-        ],
-      },
-      {
-        title: 'Hvor mange timer i uken fikk x tildelt i barnehagen',
-        name: 'fikkTildeltAntallTimer',
-        type: SanityTyper.OBJECT,
-        description: '(obligatorisk)',
-        validation: Rule => Rule.required().error('Du må fylle inn spørsmål om timer i barnehagen'),
-        fields: [
-          {
-            title: 'Spørsmål',
-            name: 'sporsmal',
-            type: CustomSanityTyper.LOCALE_STRING,
-            validation: Rule =>
-              Rule.required().error('Du må fylle inn spørsmål om timer i barnehagen'),
-          },
-          {
-            title: 'Feilmelding',
-            name: 'feilmelding',
-            type: CustomSanityTyper.LOCALE_STRING,
-            validation: Rule =>
-              Rule.required().error(
-                'Du må fylle inn en feilmelding for spørsmål om timer i barnehagen',
-              ),
-          },
-        ],
-      },
-      {
-        title: 'Hvor mange timer i uken har x fått tildelt i barnehagen',
-        name: 'fattTildeltAntallTimer',
-        type: SanityTyper.OBJECT,
-        description: '(obligatorisk)',
-        validation: Rule => Rule.required().error('Du må fylle inn spørsmål om tildelte timer'),
-        fields: [
-          {
-            title: 'Spørsmål',
-            name: 'sporsmal',
-            type: CustomSanityTyper.LOCALE_STRING,
-            validation: Rule => Rule.required().error('Du må fylle inn spørsmål om tildelte timer'),
-          },
-          {
-            title: 'Feilmelding',
-            name: 'feilmelding',
-            type: CustomSanityTyper.LOCALE_STRING,
-            validation: Rule =>
-              Rule.required().error(
-                'Du må fylle inn en feilmelding for spørsmål om tildelte timer',
               ),
           },
         ],
