ebc888962077188ca732dcc32ddf4a5da22455a8
Daphne Leebeek <daphne.leebeek@nav.no>
2022-08-24 16:33:43 +0200

    Legg til alle felter for scanning guide

--- schemas/steg/dokumentasjon/scanningGuide.ts
+++ schemas/steg/dokumentasjon/scanningGuide.ts
@@ -20,6 +20,74 @@ const scanningGuide: StegDokument = {
       description: '(obligatorisk)',
       validation: rule => rule.required().error('Du må fylle inn ekspanderbar tekst'),
     },
+    {
+      title: 'Titler og punktlister',
+      name: 'titlerOgPunktlister',
+      type: CustomSanityTyper.LOCALE_BLOCK,
+      description: '(obligatorisk)',
+      validation: rule => rule.required().error('Du må fylle inn tekst for titler og punktlister'),
+    },
+    {
+      title: 'Status på eksempler',
+      name: 'eksempelStatus',
+      type: SanityTyper.OBJECT,
+      description: '(obligatorisk)',
+      validation: rule => rule.required().error('Du må fylle inn status på eksempeler'),
+      fields: [
+        {
+          title: 'Bra',
+          name: 'bra',
+          type: CustomSanityTyper.LOCALE_STRING,
+          validation: rule => rule.required().error('Du må fylle inn status'),
+        },
+        {
+          title: 'Dårlig',
+          name: 'darlig',
+          type: CustomSanityTyper.LOCALE_STRING,
+          validation: rule => rule.required().error('Du må fylle inn status'),
+        },
+      ],
+    },
+    {
+      title: 'Beskrivelse av eksempler',
+      name: 'eksempelBeskrivelse',
+      description: '(obligatorisk)',
+      type: SanityTyper.OBJECT,
+      validation: rule => rule.required().error('Du må fylle inn beskrivelse på eksempeler'),
+      fields: [
+        {
+          title: 'Dokumentet fyller hele bilde',
+          name: 'fyllerHeleBilde',
+          type: CustomSanityTyper.LOCALE_STRING,
+          validation: rule => rule.required().error('Du må fylle inn beskrivelse'),
+        },
+        {
+          title: 'Dokumentet er ikke tatt ovenfra',
+          name: 'ikkeOvenfra',
+          type: CustomSanityTyper.LOCALE_STRING,
+          validation: rule => rule.required().error('Du må fylle inn beskrivelse'),
+        },
+        {
+          title: 'Dokumentet er ikke riktig retning',
+          name: 'feilRetning',
+          type: CustomSanityTyper.LOCALE_STRING,
+          validation: rule => rule.required().error('Du må fylle inn beskrivelse'),
+        },
+        {
+          title: 'Dokumentet har skygge',
+          name: 'skygge',
+          type: CustomSanityTyper.LOCALE_STRING,
+          validation: rule => rule.required().error('Du må fylle inn beskrivelse'),
+        },
+      ],
+    },
+    {
+      title: 'Mer hjelp-lenke',
+      name: 'merHjelpLenke',
+      type: CustomSanityTyper.LOCALE_BLOCK,
+      description: '(obligatorisk)',
+      validation: rule => rule.required().error('Du må fylle inn lenke'),
+    },
   ],
 };
 
