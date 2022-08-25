a1d47e2251a0563563157a22a41a6e0227d1bca1
Daphne Leebeek <daphne.leebeek@nav.no>
2022-08-25 09:47:37 +0200

    Legg til siste knapper og checkbox label for dokumentasjon

--- schemas/steg/dokumentasjon/knapperOgCheckbox.ts
+++ schemas/steg/dokumentasjon/knapperOgCheckbox.ts
@@ -20,6 +20,20 @@ const knapperOgCheckbox: StegDokument = {
       description: '(obligatorisk)',
       validation: rule => rule.required().error('Du må fylle inn knappetekst'),
     },
+    {
+      title: 'Fjern opplastet dokument-knapp',
+      name: 'fjern',
+      type: CustomSanityTyper.LOCALE_STRING,
+      description: '(obligatorisk)',
+      validation: rule => rule.required().error('Du må fylle inn knappetekst'),
+    },
+    {
+      title: 'Lastet opp fra før - checkbox label',
+      name: 'checkboxLabel',
+      type: CustomSanityTyper.LOCALE_STRING,
+      description: '(obligatorisk)',
+      validation: rule => rule.required().error('Du må fylle inn label tekst'),
+    },
   ],
 };
 
