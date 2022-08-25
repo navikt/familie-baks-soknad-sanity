9c5539489c3c42f2887cf2cad196e25408e013e1
Daphne <daphne.leebeek@nav.no>
2022-08-25 10:19:41 +0200

    Feat/dokumentasjon (#42)
    * Legg til dokument for scanning guide
    
    * Legg til alle felter for scanning guide
    
    * Legg til dokument for info på dokumentasjonssiden
    
    * Legg til skall for vedlegg
    
    * Legg ved vedlegg
    
    * Legg til siste knapper og checkbox label for dokumentasjon

--- schemas/steg/dokumentasjon/dokumentasjonDokumenter.ts
+++ schemas/steg/dokumentasjon/dokumentasjonDokumenter.ts
@@ -1,5 +1,9 @@
+import info from './info';
+import knapperOgCheckbox from './knapperOgCheckbox';
+import scanningGuide from './scanningGuide';
 import tittel from './tittel';
+import vedlegg from './vedlegg';
 
-const dokumentasjonDokumenter = [tittel];
+const dokumentasjonDokumenter = [tittel, info, scanningGuide, knapperOgCheckbox, vedlegg];
 
 export default dokumentasjonDokumenter;
