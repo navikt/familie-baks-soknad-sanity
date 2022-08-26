310bfa7db37a6eba7e3fd0dd3e8254615115ff8f
Daphne Leebeek <daphne.leebeek@nav.no>
2022-08-26 17:20:15 +0200

    Fiks struktur for eosForSoker

--- schemas/steg/eos-for-soker/eosForSokerDokumenter.ts
+++ schemas/steg/eos-for-soker/eosForSokerDokumenter.ts
@@ -1,6 +1,10 @@
-import spørsmål from './spørsmål';
-import tittel from './tittel';
+import localestring from '../../felles/localestring';
+import sporsmalDokument from '../../felles/sporsmalDokument';
+import { DokumentNavn, Steg } from '../../typer';
 
-const eosForSokerDokumenter = [tittel, spørsmål];
+const eosForSokerDokumenter = [
+  localestring(Steg.EØS_FOR_SØKER, DokumentNavn.EOS_FOR_SOKER_TITTEL),
+  sporsmalDokument(Steg.EØS_FOR_SØKER, DokumentNavn.EOS_FOR_SOKER_SPORSMAL),
+];
 
 export default eosForSokerDokumenter;
