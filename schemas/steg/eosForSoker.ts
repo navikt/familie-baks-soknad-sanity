aa1a1ad42a8145449a38d733b032830e0b3edb1b
Daphne <daphne.leebeek@nav.no>
2022-09-02 15:15:12 +0200

    Gjør tittel om til blokk fra string (#57)

--- schemas/steg/eosForSoker.ts
+++ schemas/steg/eosForSoker.ts
@@ -1,9 +1,9 @@
-import localeStringForStegDokument from '../felles/localeStringForStegDokument';
+import localeBlockForStegDokument from '../felles/localeBlockForStegDokument';
 import sporsmalDokument from '../felles/sporsmalDokument';
 import { DokumentNavn, Steg } from '../typer';
 
 const eosForSoker = [
-  localeStringForStegDokument(Steg.EØS_FOR_SØKER, DokumentNavn.EOS_FOR_SOKER_TITTEL),
+  localeBlockForStegDokument(Steg.EØS_FOR_SØKER, DokumentNavn.EOS_FOR_SOKER_TITTEL),
   sporsmalDokument(Steg.EØS_FOR_SØKER, DokumentNavn.EOS_FOR_SOKER_SPORSMAL),
 ];
 
