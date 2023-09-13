0fee19059b0f5d07fbf8b16f777cde690ee16366
Daphne Leebeek <daphne.leebeek@nav.no>
2023-09-13 12:26:05 +0200

    Fjerne overflødig kommentarer og rettet opp en feilmelding

--- util/client.ts
+++ util/client.ts
@@ -2,22 +2,19 @@ import { createClient } from '@sanity/client';
 
 import { Dataset, PROSJEKT_ID } from './constants';
 
-// Import using ESM URL imports in environments that supports it:
-// import {createClient} from 'https://esm.sh/@sanity/client'
 export const client = () =>
   createClient({
     projectId: PROSJEKT_ID,
     dataset: hentDatasett(),
-    useCdn: true, // set to `false` to bypass the edge cache
-    apiVersion: '2022-07-04', // use current date (YYYY-MM-DD) to target the latest API version
-    // token: process.env.SANITY_SECRET_TOKEN // Only if you want to update content with the client
+    useCdn: true,
+    apiVersion: '2022-07-04',
   });
 
 export const hentDatasett = (): Dataset => {
   const datasett = window.location.pathname.split('/')[1];
 
   if (!erDataset(datasett)) {
-    throw new Error('datasett er ikke et Datasett!');
+    throw new Error(`Datasettet: ${datasett} som forsøkes hentes fra url-path finnes ikke!`);
   }
 
   return datasett;
