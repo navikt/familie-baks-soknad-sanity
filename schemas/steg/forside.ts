73186ee5d349525c0b2d2e9131d94815d6165bd7
Ida Merete Enholm <ida.merete.enholm@nav.no>
2024-03-14 12:45:45 +0100

    Legg til nye forside-tekster for barnetrygd (#469)
    Favro:
    https://favro.com/organization/98c34fb974ce445eac854de0/1844bbac3b6605eacc8f5543?card=NAV-18565
    
    Legger til nye typer for forside-tekstene til barnetrygd, og rendrer de
    ut i editoren. Strukturen på forsiden ser nå sånn her ut:
    
    ![image](https://github.com/navikt/familie-baks-soknad-sanity/assets/25459913/c83ab0a2-a764-475b-bc5e-fddd28ce29a4)
    
    Og alt vi har lagt til her lages som tomme dokumenter som ikke er
    publisert.
    
    Parprogrammert med @daphneleebeek og @kristeine

--- schemas/steg/forside.ts
+++ schemas/steg/forside.ts
@@ -5,13 +5,20 @@ import { DokumentNavn, Steg } from '../typer';
 
 const forside = [
   localeBlockForStegDokument(Steg.FORSIDE, DokumentNavn.FORSIDE_VEILEDERHILSEN),
+  localeBlockForStegDokument(Steg.FORSIDE, DokumentNavn.FORSIDE_VEILEDERINTRO),
   localeBlockForStegDokument(Steg.FORSIDE, DokumentNavn.FORSIDE_TITTEL),
+  localeBlockForStegDokument(Steg.FORSIDE, DokumentNavn.FORSIDE_TITTEL_BARNETRYGD),
   localeBlockForStegDokument(Steg.FORSIDE, DokumentNavn.FORSIDE_SPRAKVELGER),
   localeBlockForStegDokument(Steg.FORSIDE, DokumentNavn.FORSIDE_PUNKTLISTE),
+  localeBlockForStegDokument(Steg.FORSIDE, DokumentNavn.FORSIDE_INFO_PLIKTER_TITTEL),
+  localeBlockForStegDokument(Steg.FORSIDE, DokumentNavn.FORSIDE_INFO_PLIKTER),
+  localeBlockForStegDokument(Steg.FORSIDE, DokumentNavn.FORSIDE_INFO_PERSONOPPLYSNINGER_TITTEL),
+  localeBlockForStegDokument(Steg.FORSIDE, DokumentNavn.FORSIDE_INFO_PERSONOPPLYSNINGER),
   localeBlockForStegDokument(Steg.FORSIDE, DokumentNavn.FORSIDE_BEKREFTELSESBOKS_BRODTEKST),
   localeStringForStegDokument(Steg.FORSIDE, DokumentNavn.FORSIDE_BEKREFTELSESBOKS_ANDRE_TEKSTER),
   localeBlockForStegDokument(Steg.FORSIDE, DokumentNavn.FORSIDE_PERSONOPPLYSNINGSLENKE),
   localeBlockForStegDokument(Steg.FORSIDE, DokumentNavn.FORSIDE_MELLOMLAGRET_ALERT),
+  localeBlockForStegDokument(Steg.FORSIDE, DokumentNavn.FORSIDE_UTVIDET_BARNETRYGD_ALERT),
   sporsmalDokument(Steg.FORSIDE, DokumentNavn.FORSIDE_SPORSMAL),
 ];
 
