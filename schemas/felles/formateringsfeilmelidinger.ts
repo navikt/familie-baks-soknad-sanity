7f2c44ef8b9819fb64fcb4c3f3a354432dff4bb1
Kristine Steine <kristine.steine@bekk.no>
2024-04-19 09:44:48 +0200

    Legg til todo i formateringsfeilmeldinger

--- schemas/felles/formateringsfeilmelidinger.ts
+++ schemas/felles/formateringsfeilmelidinger.ts
@@ -4,6 +4,7 @@ import localeStringForStegDokument from './localeStringForStegDokument';
 
 const formateringsfeilmelidinger = [
   localeStringForStegDokument(Steg.FELLES, DokumentNavn.FORMATERINGSFEILMELDINGER),
+  // TODO endre til localeBlockForStegDokument, sikre at eksisterende tekster bevares
 ];
 
 export default formateringsfeilmelidinger;
