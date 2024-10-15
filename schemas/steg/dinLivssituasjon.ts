1317c83c215ea69d41a29e1f2c3aee63640d636b
ole-kristian-rudjord <olekristianrudjord@gmail.com>
2024-10-15 10:07:18 +0200

    Legger til Årsak alternativer i Din livssituasjon (DIN_LIVSSITUASJON_ARSAK_ALTERNATIVER)

--- schemas/steg/dinLivssituasjon.ts
+++ schemas/steg/dinLivssituasjon.ts
@@ -1,4 +1,5 @@
 import localeBlockForStegDokument from '../felles/localeBlockForStegDokument';
+import localeStringForStegDokument from '../felles/localeStringForStegDokument';
 import sporsmalDokument from '../felles/sporsmalDokument';
 import { DokumentNavn, Steg } from '../typer';
 
@@ -6,6 +7,10 @@ const dinLivssituasjon = [
   localeBlockForStegDokument(Steg.DIN_LIVSSITUASJON, DokumentNavn.DIN_LIVSSITUASJON_TITTEL),
   localeBlockForStegDokument(Steg.DIN_LIVSSITUASJON, DokumentNavn.DIN_LIVSSITUASJON_GUIDE),
   sporsmalDokument(Steg.DIN_LIVSSITUASJON, DokumentNavn.DIN_LIVSSITUASJON_SPORSMAL),
+  localeStringForStegDokument(
+    Steg.DIN_LIVSSITUASJON,
+    DokumentNavn.DIN_LIVSSITUASJON_ARSAK_ALTERNATIVER,
+  ),
 ];
 
 export default dinLivssituasjon;
