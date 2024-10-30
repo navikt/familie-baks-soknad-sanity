2453e2922f819cc503634e44da9a251f561d3ab0
ole-kristian-rudjord <olekristianrudjord@gmail.com>
2024-10-30 13:12:41 +0100

    Legger til tittel tekst for tidligere samboere modal

--- schemas/felles/modaler/tidligereSamboereModal.ts
+++ schemas/felles/modaler/tidligereSamboereModal.ts
@@ -2,6 +2,7 @@ import { DokumentNavn, Steg } from '../../typer';
 import localeBlockForStegDokument from '../localeBlockForStegDokument';
 
 const tidligereSamboereModal = [
+  localeBlockForStegDokument(Steg.FELLES, DokumentNavn.MODAL_TIDLIGERE_SAMBOERE_TITTEL_SOKER),
   localeBlockForStegDokument(
     Steg.FELLES,
     DokumentNavn.MODAL_TIDLIGERE_SAMBOERE_ANDRE_TEKSTER_SOKER,
