c6d8426f27118c570571131567cedb213a98b68c
ole-kristian-rudjord <olekristianrudjord@gmail.com>
2024-10-30 09:20:23 +0100

    Legger til tittel i barnetrygdsperiode modal

--- schemas/felles/modaler/barnetrygdsperiodeModal.ts
+++ schemas/felles/modaler/barnetrygdsperiodeModal.ts
@@ -2,6 +2,15 @@ import { DokumentNavn, Steg } from '../../typer';
 import localeBlockForStegDokument from '../localeBlockForStegDokument';
 
 const barnetrygdsperiodeModal = [
+  localeBlockForStegDokument(Steg.FELLES, DokumentNavn.MODAL_BARNETRYGDSPERIODE_TITTEL_SOKER),
+  localeBlockForStegDokument(
+    Steg.FELLES,
+    DokumentNavn.MODAL_BARNETRYGDSPERIODE_TITTEL_ANDRE_FORELDER,
+  ),
+  localeBlockForStegDokument(
+    Steg.FELLES,
+    DokumentNavn.MODAL_BARNETRYGDSPERIODE_TITTEL_OMSORGSPERSON,
+  ),
   localeBlockForStegDokument(
     Steg.FELLES,
     DokumentNavn.MODAL_BARNETRYGDSPERIODE_ANDRE_TEKSTER_SOKER,
