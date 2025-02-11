787a29ba10e23d11fc28c238ae979993d03a1066
Ole Kristian Rudjord <89088327+ole-kristian-rudjord@users.noreply.github.com>
2025-02-11 16:22:31 +0100

    Bytter fra localeBlockForStegDokument til å bruke sporsmalDokument for spørsmål i barnetrygdsperiode (#609)
    Retter en feil som ble gjort i denne oppgaven:
    https://github.com/navikt/familie-baks-soknad-sanity/pull/608

--- schemas/felles/modaler/barnetrygdsperiodeModal.ts
+++ schemas/felles/modaler/barnetrygdsperiodeModal.ts
@@ -1,5 +1,6 @@
 import { DokumentNavn, Steg } from '../../typer';
 import localeBlockForStegDokument from '../localeBlockForStegDokument';
+import sporsmalDokument from '../sporsmalDokument';
 
 const barnetrygdsperiodeModal = [
   localeBlockForStegDokument(Steg.FELLES, DokumentNavn.MODAL_BARNETRYGDSPERIODE_TITTEL_SOKER),
@@ -11,15 +12,9 @@ const barnetrygdsperiodeModal = [
     Steg.FELLES,
     DokumentNavn.MODAL_BARNETRYGDSPERIODE_TITTEL_OMSORGSPERSON,
   ),
-  localeBlockForStegDokument(Steg.FELLES, DokumentNavn.MODAL_BARNETRYGDSPERIODE_SPORSMAL_SOKER),
-  localeBlockForStegDokument(
-    Steg.FELLES,
-    DokumentNavn.MODAL_BARNETRYGDSPERIODE_SPORSMAL_ANDRE_FORELDER,
-  ),
-  localeBlockForStegDokument(
-    Steg.FELLES,
-    DokumentNavn.MODAL_BARNETRYGDSPERIODE_SPORSMAL_OMSORGSPERSON,
-  ),
+  sporsmalDokument(Steg.FELLES, DokumentNavn.MODAL_BARNETRYGDSPERIODE_SPORSMAL_SOKER),
+  sporsmalDokument(Steg.FELLES, DokumentNavn.MODAL_BARNETRYGDSPERIODE_SPORSMAL_ANDRE_FORELDER),
+  sporsmalDokument(Steg.FELLES, DokumentNavn.MODAL_BARNETRYGDSPERIODE_SPORSMAL_OMSORGSPERSON),
   localeBlockForStegDokument(
     Steg.FELLES,
     DokumentNavn.MODAL_BARNETRYGDSPERIODE_ANDRE_TEKSTER_SOKER,
