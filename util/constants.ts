7962e37bd6b46549b5a7a2472b6e2c84e5665bc7
Uy Nguyen <uy.quang.nguyen@nav.no>
2025-08-04 08:08:32 +0200

    Rydd opp i ubrukte sanity prosjekter, rename KS datasets

--- util/constants.ts
+++ util/constants.ts
@@ -1,9 +1,8 @@
 export const PROSJEKT_ID = 'by26nl8j';
 
 export enum Dataset {
-  PROD = 'production',
-  PROD_V2 = 'production-v2',
-  TEST = 'test',
+  KS_PROD = 'ks-production',
+  KS_TEST = 'ks-test',
   BA_PROD = 'ba-production',
   BA_TEST = 'ba-test',
 }
